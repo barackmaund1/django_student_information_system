81fa9e932cf34c7ce25384fba2cdc14a1ed262dd
Schwart <conallmcginty@gmail.com>
2018-05-25 14:12:27 +0300

    Create announcement model for the notice_board app, migrate the changes to the database, along with initial migrations.

--- SIS/notice_board/models.py
+++ SIS/notice_board/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Announcement(models.Model):
+    created = models.DateTimeField(default=timezone.now)
+    header = models.CharField(max_length=50)
+    body_text = models.TextField(max_length=500)
