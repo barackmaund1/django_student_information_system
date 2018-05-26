1759b7a7fadee5cb687e0413dd72fd3a89b2870f
Schwart <conallmcginty@gmail.com>
2018-05-26 11:28:01 +0300

    Change AnnouncementFactory header and body_text attributes to lazy_attributes, as currently they display the same information each time they are called as they are set once when the class is imported. Tests still pass as expected.

--- SIS/notice_board/factories.py
+++ SIS/notice_board/factories.py
@@ -1,10 +1,15 @@
 from factory import DjangoModelFactory
-from factory import Faker
+from factory import lazy_attribute
 from notice_board.models import Announcement
+import faker
+
+faker = faker.Factory.create()
 
 class AnnouncementFactory(DjangoModelFactory):
     class Meta:
         model = Announcement
 
-    header = Faker('text').generate(extra_kwargs={'max_nb_chars': 50})
-    body_text = Faker('text').generate(extra_kwargs={'max_nb_chars': 500})
+    # header = Faker('text').generate(extra_kwargs={'max_nb_chars': 50})
+    header = lazy_attribute(lambda x: faker.text(max_nb_chars=50))
+    # body_text = Faker('text').generate(extra_kwargs={'max_nb_chars': 500})
+    body_text = lazy_attribute(lambda x: faker.text(max_nb_chars=500))
