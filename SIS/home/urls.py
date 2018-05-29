7e0247ef7b85e89aeb185378f025e18781631d69
Schwart <conallmcginty@gmail.com>
2018-05-29 17:06:33 +0300

    Add app_name setting to home/urls.py to allow for namespace usage in main URLconf. Configure logout view in home/urls.py

--- SIS/home/urls.py
+++ SIS/home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from home.views import Home
+from home.views import Home, Logout
+
+app_name = 'home'
 
 urlpatterns = [
-    path('', Home.as_view(), name='home-page')
+    path('', Home.as_view(), name='home-page'),
+    path('logout', Logout.as_view(), name='logout')
 ]
