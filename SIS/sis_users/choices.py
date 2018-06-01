e48aae6b7830c467ce0f12cf418ffc6aebaaf349
Schwart <conallmcginty@gmail.com>
2018-06-01 13:18:38 +0300

    Create Staff and Student models
    Create a staff model with a OneToOne field to the User model, also with a boolean flag that signals if the staff member has admin access or not.
    
    Create a student model with a OneToOne field to the User, a class group and a year group.
    Add a function to get the full class name for display purposes.

---
+++ SIS/sis_users/choices.py
@@ -0,0 +1,14 @@
+CLASS_CHOICES = (
+    ('T1', 'Technical - Set 1'),
+    ('T2', 'Technical - Set 2'),
+    ('T3', 'Technical - Set 3'),
+    ('V1', 'Vocational - Set 1'),
+    ('V2', 'Vocational - Set 2'),
+    ('V3', 'Vocational - Set 3')
+)
+
+YEAR_GROUP_CHOICES = (
+    (10, 'Grade 10'),
+    (11, 'Grade 11'),
+    (12, 'Grade 12')
+)
--- SIS/sis_users/models.py
+++ SIS/sis_users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from sis_users.choices import CLASS_CHOICES, YEAR_GROUP_CHOICES
 
-# Create your models here.
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    class_group = models.CharField(max_length=4, choices=CLASS_CHOICES, null=True)
+    year_group = models.IntegerField(choices=YEAR_GROUP_CHOICES, null=True)
+
+    def get_full_class_name(self, user_id):
+        user = User.objects.get(pk=user_id)
+        year_group = user.year_group
+        class_group = user.class_group
+        return f'{year_group} - {class_group}'
+
