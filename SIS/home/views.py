0e160eb1ba6b827d820d0d9c7f45cf9731132a55
Schwart <conallmcginty@gmail.com>
2018-06-01 13:27:27 +0300

    Add user_state variable to context to display a message to the user
    Two things could happen:
    - if the user is anonymous, then only announcements will be displayed on the page.
    - If the user is logged in, a user_state variable will be added to the context, indicating that they are either staff or student.

--- SIS/home/views.py
+++ SIS/home/views.py
@@ -4,14 +4,24 @@ from notice_board.models import Announcement
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
+from sis_users.models import Staff, Student
 
 class Home(View):
     def get(self, request):
         annoucements = Announcement.objects.order_by('-created')[:5]
+        user = request.user
+
         context = {
             'page_title': 'home page',
-            'announcements': annoucements
+            'announcements': annoucements,
         }
+        if not request.user.is_anonymous:
+            if hasattr(user, 'staff'):
+                user_state = 'staff'
+            elif hasattr(user, 'student'):
+                user_state = 'student'
+            context['user_state'] = user_state
+
         return render(request, "home.html", context)
 
 class Logout(View):
