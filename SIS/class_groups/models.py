77b73200a56af4a334951d6d24adab382f7d7f54
Schwart <conallmcginty@gmail.com>
2018-06-02 14:27:52 +0300

    Migrate new choice changes, along with additional fields
    Class groups are now more granular with how the class is defined (as per my school's system, this could be changed for others).
    - Year group (10, 11, 12)
    - Band (technical / vocational)
    - Set (1, 2, 3)

--- SIS/class_groups/models.py
+++ SIS/class_groups/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-from sis_users.choices import CLASS_CHOICES, YEAR_GROUP_CHOICES
+from class_groups.choices import YEAR_CHOICES, BAND_CHOICES, SET_CHOICES
 
 class Subject(models.Model):
     subject_name = models.CharField(max_length=20, null=True)
+    teachers = models.ManyToManyField('sis_users.Staff')
 
 class ClassGroup(models.Model):
-    year_group = models.IntegerField(choices=YEAR_GROUP_CHOICES, null=False)
-    class_name = models.CharField(max_length=2, choices=CLASS_CHOICES, null=False)
+    year_group = models.IntegerField(choices=YEAR_CHOICES, null=False)
+    band = models.CharField(max_length=1, choices=BAND_CHOICES, null=False)
+    set = models.IntegerField(choices=SET_CHOICES, null=False)
     subjects = models.ManyToManyField(Subject)
     teachers = models.ManyToManyField('sis_users.Staff')
