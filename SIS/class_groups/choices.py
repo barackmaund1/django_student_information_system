b44054818296b171fc0689925e62a7e93aa8afc6
Schwart <conallmcginty@gmail.com>
2018-06-02 14:24:23 +0300

    Create new choices file for class groups
    Create the students / staff first was the wrong way round. The class groups should have been created first, then the users should have been created with those decisions in mind.

---
+++ SIS/class_groups/choices.py
@@ -0,0 +1,16 @@
+YEAR_CHOICES = (
+    (10, 'Grade 10'),
+    (11, 'Grade 11'),
+    (12, 'Grade 12')
+)
+
+BAND_CHOICES = (
+    ('T', 'Technical'),
+    ('V', 'Vocational')
+)
+
+SET_CHOICES = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3')
+)
