4854853cd77a1a56848ef224b6cf996bf8d5fd35
Schwart <conallmcginty@gmail.com>
2018-06-02 16:31:12 +0300

    Create stubs for class and subject tests

--- SIS/class_groups/tests.py
+++ SIS/class_groups/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from class_groups.models import ClassGroup, Subject
 
 # Create your tests here.
+class ClassTestCase(TestCase):
+    def test_class_creation(self):
+        pass
+
+class SubjectTestCase(TestCase):
+    def test_subject_creation(self):
+        pass
