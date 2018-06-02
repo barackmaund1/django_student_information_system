f2073f20a52d2c50082ba27549ad9b82f9f934ab
Schwart <conallmcginty@gmail.com>
2018-06-02 11:44:52 +0300

    change TextField to CharField in ClassGroup

--- SIS/class_groups/models.py
+++ SIS/class_groups/models.py
@@ -4,4 +4,4 @@ from sis_users.choices import CLASS_CHOICES, YEAR_GROUP_CHOICES
 
 class ClassGroup(models.Model):
     year_group = models.IntegerField(choices=YEAR_GROUP_CHOICES, null=False)
-    class_name = models.TextField(max_length=2, choices=CLASS_CHOICES, null=False)
+    class_name = models.CharField(max_length=2, choices=CLASS_CHOICES, null=False)
