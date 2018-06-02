be985337b211f9dced1daddd74ff70a5e5e27c87
Schwart <conallmcginty@gmail.com>
2018-06-02 11:47:49 +0300

    Create subjects model

--- SIS/class_groups/models.py
+++ SIS/class_groups/models.py
@@ -5,3 +5,6 @@ from sis_users.choices import CLASS_CHOICES, YEAR_GROUP_CHOICES
 class ClassGroup(models.Model):
     year_group = models.IntegerField(choices=YEAR_GROUP_CHOICES, null=False)
     class_name = models.CharField(max_length=2, choices=CLASS_CHOICES, null=False)
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=20, null=True)
