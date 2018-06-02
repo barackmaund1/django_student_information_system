30675c536b6888319ef2553f9dce57ab3e849fa9
Schwart <conallmcginty@gmail.com>
2018-06-02 11:09:08 +0300

    Create test ensuring student instance is created properly
    Student state is created, then a user with the same email address is created.
    The returned user should have a student attribute with the same values as the state.

--- SIS/sis_users/tests.py
+++ SIS/sis_users/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sis_users.factories import StudentStateFactory, StaffStateFactory, UserFactory
+
+class PostSaveTestCase(TestCase):
+    '''
+        Test that the post_save hooks are working correctly
+        ie. making the correct instance based on the info from the state table
+    '''
+    def test_student_instance(self):
+        '''
+            Create a student state in the database,
+            then create a user with the same email address
+            assert that the user has a student instance
+            wtih the correct attributes (class_group, year_group)
+        '''
+        state = StudentStateFactory()
+        user = UserFactory(email=state.email_address)
+
+        assert hasattr(user, 'student')
+        student = user.student
+
+        self.assertEqual(student.class_group, state.class_group)
+        self.assertEqual(student.year_group, state.year_group)
