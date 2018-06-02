3b7f79619fa0358368fca9a2224ef07b093c4d0c
Schwart <conallmcginty@gmail.com>
2018-06-02 14:41:01 +0300

    Change year_group to year in ClassGroup model

--- SIS/class_groups/models.py
+++ SIS/class_groups/models.py
@@ -7,7 +7,7 @@ class Subject(models.Model):
     teachers = models.ManyToManyField('sis_users.Staff')
 
 class ClassGroup(models.Model):
-    year_group = models.IntegerField(choices=YEAR_CHOICES, null=False)
+    year = models.IntegerField(choices=YEAR_CHOICES, null=False)
     band = models.CharField(max_length=1, choices=BAND_CHOICES, null=False)
     set = models.IntegerField(choices=SET_CHOICES, null=False)
     subjects = models.ManyToManyField(Subject)
