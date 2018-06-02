24000d25020377f0d821bde965023d74f9c4e9e4
Schwart <conallmcginty@gmail.com>
2018-06-02 10:24:21 +0300

    Create student / staff state factories
    Factories will generate an instance in the state table, which will be used to pull data from when the user is saved.

--- SIS/sis_users/factories.py
+++ SIS/sis_users/factories.py
@@ -1,6 +1,11 @@
 from factory import DjangoModelFactory, lazy_attribute, SubFactory
 from django.contrib.auth.models import User
 import faker
+from random import choice
+
+from sis_users.models import Staff, Student, UserState
+from sis_users.choices import CLASS_CHOICES, YEAR_GROUP_CHOICES
+
 faker = faker.Factory.create()
 
 class UserFactory(DjangoModelFactory):
@@ -12,3 +17,19 @@ class UserFactory(DjangoModelFactory):
     last_name = lazy_attribute(lambda x: faker.last_name())
     email = lazy_attribute(lambda x: faker.email())
     password = lazy_attribute(lambda x: faker.password())
+
+class StaffStateFactory(DjangoModelFactory):
+    class Meta:
+        model = UserState
+
+    email_address = lazy_attribute(lambda x: faker.email())
+    staff = True
+
+class StudentStateFactory(DjangoModelFactory):
+    class Meta:
+        model = UserState
+
+    email_address = lazy_attribute(lambda x: faker.email())
+    staff = False
+    year_group = lazy_attribute(lambda x: choice([c[0] for c in YEAR_GROUP_CHOICES]))
+    class_group = lazy_attribute(lambda x: choice([c[0] for c in CLASS_CHOICES]))
