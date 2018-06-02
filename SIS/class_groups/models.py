3a4c140278ff9172d7596831380a9dbd041c1f4d
Schwart <conallmcginty@gmail.com>
2018-06-02 11:52:19 +0300

    Add ManyToMany field to ClassGroup
    Also move Subject above ClassGroup to ensure reference is resolved.

--- SIS/class_groups/models.py
+++ SIS/class_groups/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 from sis_users.choices import CLASS_CHOICES, YEAR_GROUP_CHOICES
 
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=20, null=True)
+
 class ClassGroup(models.Model):
     year_group = models.IntegerField(choices=YEAR_GROUP_CHOICES, null=False)
     class_name = models.CharField(max_length=2, choices=CLASS_CHOICES, null=False)
-
-class Subject(models.Model):
-    subject_name = models.CharField(max_length=20, null=True)
+    subjects = models.ManyToManyField(Subject)
