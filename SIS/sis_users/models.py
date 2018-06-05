99006e524f7345796ffaa08eaeee4b2ffed8c383
Schwart <conallmcginty@gmail.com>
2018-06-05 20:43:30 +0300

    Add __str__ to Admin model.

--- SIS/sis_users/models.py
+++ SIS/sis_users/models.py
@@ -8,6 +8,9 @@ from class_groups.choices import YEAR_CHOICES, BAND_CHOICES, SET_CHOICES
 class Admin(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Admin: {self.user.email}'
+
 class Staff(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
