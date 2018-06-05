56bc1943d6a90d89825b5d147c9be309074d2a81
Schwart <conallmcginty@gmail.com>
2018-06-05 16:34:04 +0300

    Uncomment class_group retrieval

--- SIS/sis_users/models.py
+++ SIS/sis_users/models.py
@@ -50,14 +50,12 @@ def create_student_or_staff(sender, instance, created, **kwargs):
         else:
             class_instance = None
             if state.year and state.band and state.set:
-                '''
                 class_model = apps.get_model('class_groups.ClassGroup')
                 class_instance = class_model.objects.get(
                     year=state.year,
                     band=state.band,
                     set=state.set
                 )
-                '''
             Student.objects.create(
                 user=instance,
                 class_group=class_instance
