fe19e382766199fa92280ff6da2aa678d244041c
Schwart <conallmcginty@gmail.com>
2018-06-06 18:39:33 +0300

    Create basic view for user profile

--- SIS/sis_users/views.py
+++ SIS/sis_users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.urls import reverse
+from sis_users.mixins import LoginRequiredMessageMixin
 
-# Create your views here.
+class Profile(LoginRequiredMessageMixin, View):
+    login_url = '/'
+    error_message = 'You need to login using your school email address.'
+
+    def get(self, request):
+        return render(request, 'sis_users/profile.html')
+
