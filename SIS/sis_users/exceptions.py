60387d1540db243e6e4e27391e53b6653bf68f3e
Schwart <conallmcginty@gmail.com>
2018-06-08 14:09:29 +0300

    Change exception message.

--- SIS/sis_users/exceptions.py
+++ SIS/sis_users/exceptions.py
@@ -2,4 +2,4 @@ from social_core.exceptions import AuthException
 
 class UserStateDoesNotExistException(AuthException):
     def __str__(self):
-        return "You must be an administrator, staff member or a student to sign in."
+        return "You must be an administrator, staff member or a student to sign in. Please contact the school for more assistance."
