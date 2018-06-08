f03eff619c4dfee92bb596b6afad6ffceec55e2c
Schwart <conallmcginty@gmail.com>
2018-06-08 13:34:14 +0300

    Chnage UserStateNotPresentException to UserStateDoesNotExistException
    More accurately describes why the exception was raised.

--- SIS/sis_users/exceptions.py
+++ SIS/sis_users/exceptions.py
@@ -1,5 +1,5 @@
 from social_core.exceptions import AuthException
 
-class UserStateNotPresentException(AuthException):
+class UserStateDoesNotExistException(AuthException):
     def __str__(self):
         return "You must be an administrator, staff member or a student to sign in."
