66cf6c436997a984d33c8f211e0d747886ba9ae7
Schwart <conallmcginty@gmail.com>
2018-06-10 18:00:52 +0300

    Recreate YearView with DetailView, present years/bands/sets in year_class_page.html

--- SIS/class_groups/views.py
+++ SIS/class_groups/views.py
@@ -1,8 +1,15 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
+from django.conf import settings
 from sis_users.mixins import LoginRequiredMessageMixin
+from class_groups.models import School, Year, Band, Set
 
-class SchoolView(LoginRequiredMessageMixin, TemplateView):
+class SchoolView(LoginRequiredMessageMixin, DetailView):
     template_name = 'class_groups/school_class_page.html'
+    model = School
+
+    def get_object(self, queryset=None):
+        # get the name of the school defined in settings / environment variable
+        return School.objects.get(name=settings.SCHOOL_NAME)
 
 class YearView(LoginRequiredMessageMixin, TemplateView):
     template_name = 'class_groups/year_class_page.html'
