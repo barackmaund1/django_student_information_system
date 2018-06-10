a4d1669906a2f3180290637b5a6334688d14cec2
Schwart <conallmcginty@gmail.com>
2018-06-10 11:54:00 +0300

    Create a school view template

--- SIS/class_groups/views.py
+++ SIS/class_groups/views.py
@@ -1,12 +1,18 @@
 from django.views.generic import ListView
 from sis_users.mixins import LoginRequiredMessageMixin
 from class_groups.models import ClassGroup
+from class_groups.choices import YEAR_CHOICES, BAND_CHOICES, SET_CHOICES
 
 class SchoolView(LoginRequiredMessageMixin, ListView):
-    template_name = 'class_groups/genericlistview.html'
+    template_name = 'class_groups/school_view.html'
+    queryset = ClassGroup.objects.all()
 
-    def get_queryset(self):
-        return ClassGroup.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['years'] = YEAR_CHOICES
+        context['bands'] = BAND_CHOICES
+        context['sets'] = SET_CHOICES
+        return context
 
 class YearView(LoginRequiredMessageMixin, ListView):
     template_name = 'class_groups/genericlistview.html'
