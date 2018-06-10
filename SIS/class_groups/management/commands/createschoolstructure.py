445d840fb90270f7f0637010c9bc0d3f48a3fb6b
Schwart <conallmcginty@gmail.com>
2018-06-10 16:19:52 +0300

    Create management command to create school structure.
    Creates a school, all requires years, bands and sets by running "python manage.py createschoolstructure"

--- SIS/class_groups/management/commands/createschoolstructure.py
+++ SIS/class_groups/management/commands/createschoolstructure.py
@@ -18,4 +18,24 @@ class Command(BaseCommand):
     '''
     def handle(self, *args, **options):
         school_name = settings.SCHOOL_NAME
+        school = School.objects.create(name=school_name)
+        years = [Year(value=v) for v in YEAR_CHOICES]
+        [year.save() for year in years]
+        for year in years:
+            school.year_set.add(year)
+            school.save()
+            bands = [Band(value=v) for v in BAND_CHOICES]
+            [band.save() for band in bands]
+            for band in bands:
+                year.band_set.add(band)
+                year.save()
+                sets = [Set(value=v) for v in SET_CHOICES]
+                [set.save() for set in sets]
+                for set in sets:
+                    band.set_set.add(set)
+                    band.save()
+        created_sets = Set.objects.all()
+        self.stdout.write(f'Created the following {created_sets.count()} classes:')
+        for set in created_sets:
+            self.stdout.write(set.__str__())
 
