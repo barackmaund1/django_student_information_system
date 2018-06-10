a4166fdd7455570084fa23e8bff52aeb4076c612
Schwart <conallmcginty@gmail.com>
2018-06-10 12:09:04 +0300

    Change YearView get_context_data() and subclass TemplateView instead
    Don't need queryset to list all the classes for a particular year group.

--- SIS/class_groups/views.py
+++ SIS/class_groups/views.py
@@ -13,12 +13,15 @@ class SchoolView(LoginRequiredMessageMixin, TemplateView):
         context['sets'] = SET_CHOICES
         return context
 
-class YearView(LoginRequiredMessageMixin, ListView):
-    template_name = 'class_groups/school_class_page.html.html'
+class YearView(LoginRequiredMessageMixin, TemplateView):
+    template_name = 'class_groups/year_class_page.html'
 
-    def get_queryset(self):
-        year = self.kwargs['year']
-        return ClassGroup.objects.filter(year=year)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['year'] = tuple(filter(lambda x: x[0] == self.kwargs['year'], YEAR_CHOICES))
+        context['bands'] = BAND_CHOICES
+        context['sets'] = SET_CHOICES
+        return context
 
 class BandView(LoginRequiredMessageMixin, ListView):
     template_name = 'class_groups/base_class_page.html'
