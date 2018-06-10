e28749c8159605c85262f95e5ac24f997a5851f1
Schwart <conallmcginty@gmail.com>
2018-06-10 18:10:25 +0300

    Add all bands/sets to appropriate YearView template

--- SIS/class_groups/views.py
+++ SIS/class_groups/views.py
@@ -11,8 +11,14 @@ class SchoolView(LoginRequiredMessageMixin, DetailView):
         # get the name of the school defined in settings / environment variable
         return School.objects.get(name=settings.SCHOOL_NAME)
 
-class YearView(LoginRequiredMessageMixin, TemplateView):
+class YearView(LoginRequiredMessageMixin, DetailView):
     template_name = 'class_groups/year_class_page.html'
+    model = Year
+
+    def get_object(self, queryset=None):
+        return Year.objects.get(
+            value=self.kwargs['year']
+        )
 
 class BandView(LoginRequiredMessageMixin, TemplateView):
     template_name = 'class_groups/band_class_page.html'
