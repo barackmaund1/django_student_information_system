01ee3492c4a10a94ed60dfc4d5c07bfcc0ccd88e
Schwart <conallmcginty@gmail.com>
2018-06-12 17:12:54 +0300

    Create View/Template for Admin/Student/Staff lists

--- SIS/sis_users/urls.py
+++ SIS/sis_users/urls.py
@@ -13,10 +13,10 @@ app_name = 'sis_users'
 
 urlpatterns = [
     path('profile', Profile.as_view(), name='profile'),
-    path('admin', AdminList.as_view(), name='admin-list'),
-    path('admin/<username>', AdminDetail.as_view(), name='admin-detail'),
+    path('admins', AdminList.as_view(), name='admin-list'),
+    path('admins/<username>', AdminDetail.as_view(), name='admin-detail'),
     path('staff', StaffList.as_view(), name='staff-list'),
     path('staff/<username>', StaffDetail.as_view(), name='staff-detail'),
-    path('student', StudentList.as_view(), name='student-list'),
-    path('student/<username>', StudentDetail.as_view(), name='student-detail'),
+    path('students', StudentList.as_view(), name='student-list'),
+    path('students/<username>', StudentDetail.as_view(), name='student-detail'),
 ]
--- SIS/sis_users/views.py
+++ SIS/sis_users/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 from django.urls import reverse
 from sis_users.mixins import LoginRequiredMessageMixin
+from sis_users.models import Admin, Staff, Student
 
 class Profile(LoginRequiredMessageMixin, View):
     def get(self, request):
         return render(request, 'sis_users/profile.html')
 
+class AdminList(LoginRequiredMessageMixin, ListView):
+    template_name = 'sis_users/admin_list.html'
+
+    def get_queryset(self):
+        return Admin.objects.all()
+
+class StaffList(LoginRequiredMessageMixin, ListView):
+    template_name = 'sis_users/staff_list.html'
+
+    def get_queryset(self):
+        return Staff.objects.all()
+
+class StudentList(LoginRequiredMessageMixin, ListView):
+    def get_queryset(self):
+        return Student.objects.all()
+
+class AdminDetail(LoginRequiredMessageMixin, DetailView):
+    def get_object(self, queryset=None):
+        return Admin.objects.get(user__username=self.kwargs['username'])
+
+class StaffDetail(LoginRequiredMessageMixin, DetailView):
+    def get_object(self, queryset=None):
+        return Staff.objects.get(user__username=self.kwargs['username'])
+
+class StudentDetail(LoginRequiredMessageMixin, DetailView):
+    def get_object(self, queryset=None):
+        return Student.objects.get(user__username=self.kwargs['username'])
