d5ae8a9c1a598c9df65dbecc1647491f9df40b62
Schwart <conallmcginty@gmail.com>
2018-06-12 16:33:20 +0300

    Update the checks for the user state in HomeView
    User can now have multiple states (Admin and Staff) so this needs to be reflected in the context. This is not going to last for much longer it's just an easy way to check the user has been created correctly.

--- SIS/home/views.py
+++ SIS/home/views.py
@@ -15,19 +15,17 @@ class Home(View):
             'page_title': 'home page',
             'announcements': annoucements,
         }
+        states = []
         if not request.user.is_anonymous:
             if hasattr(user, 'staff'):
-                user_state = 'staff'
-            elif hasattr(user, 'student'):
-                user_state = 'student'
-            elif hasattr(user, 'admin'):
-                user_state = 'admin'
-            else:
-                if user.is_superuser:
-                    user_state = 'super user'
-                else:
-                    user_state = ''
-            context['user_state'] = user_state
+                states.append('staff')
+            if hasattr(user, 'student'):
+                states.append('student')
+            if hasattr(user, 'admin'):
+                states.append('admin')
+            if user.is_superuser:
+                states.append('super-user')
+            context['user_state'] = '/'.join(states)
 
         return render(request, "home/home.html", context)
 
