ebdd7bf1d7c7ca72c3368453e7b1514268e03745
Schwart <conallmcginty@gmail.com>
2018-06-14 07:45:36 +0300

    Rewrote PostSave tests using a base class instead of individual function tests
    Make the code easier to read, and easier to write.

--- SIS/sis_users/tests.py
+++ SIS/sis_users/tests.py
@@ -9,86 +9,82 @@ from sis_users.factories import (
 )
 from class_groups.factories import SetFactory
 
-class PostSaveTestCase(TestCase):
-    '''
-        Test that the post_save hooks are working correctly
-        ie. making the correct instance based on the info from the state table
-    '''
+class BasePostSaveTestCase(TestCase):
+    def setUp(self):
+        '''
+            Setup generic stuff here that will be common for all test cases
+        '''
+        state_class = self.get_state_class()
 
-    def test_admin_instance(self):
-        '''
-            AdminStateFactory creates a state, then a UserFactory creates a
-            user using the same email address.
-            The user should have an admin instance attached, currently there
-            are no attribute values to check, until more functionality is
-            given to the Admin model.
-        '''
-        # create an admin state
-        state = AdminStateFactory()
+        if state_class.__name__ == 'StudentStateFactory':
+            set = SetFactory()
+            self.state = state_class(
+                year=set.band.year.value,
+                band=set.band.value,
+                set=set.value
+            )
+        else:
+            self.state = state_class()
         # create a user
-        user = UserFactory(email=state.email)
-        # retrieve the same user from the db
-        user_from_db = User.objects.get(
-            email=state.email
-        )
-        # check the instance has an 'admin' attribute
-        assert hasattr(user_from_db, 'admin')
-
-    def test_staff_model(self):
-        '''
-            Same process as Admin model, but with the Staff model instead.
-        '''
-        # create a staff state
-        state = StaffStateFactory()
-        # create a user
-        user = UserFactory(email=state.email)
-        # retrieve that user from the db
-        user_from_db = User.objects.get(
-            email=state.email
-        )
-        # check the instance has a 'staff' attribute
-        assert hasattr(user_from_db, 'staff')
-
-    def test_student_instance(self):
-        '''
-            StudentStateFactory creates a state, then a UserFactory creates
-            a user using the same email address.
-            The user should have a link with a student instance, with the same
-            attributes and values as the user from the database.
-        '''
-
-        # create a class group
-        class_group = SetFactory()
-        # create a student state with the same class attributes
-        state = StudentStateFactory(
-            year=class_group.band.year.value,
-            band=class_group.band.value,
-            set=class_group.value
-        )
-        # create a user with the same email as the state
-        user = UserFactory(email=state.email)
+        user = UserFactory(email=self.state.email)
         # get the user from the database
-        user_from_db = User.objects.get(
-            email=state.email
+        self.user = User.objects.get(
+            email=self.state.email
         )
-        # assert it has a student instance
-        assert hasattr(user_from_db, 'student')
+
+    def get_state_class(self):
+        if not hasattr(self, 'state_class'):
+            raise self.fail('Must define a state_class for each subclass.')
+        else:
+            return self.state_class
+
+class AdminPostSaveTestCase(BasePostSaveTestCase):
+    state_class = AdminStateFactory
+
+    def test_admin_post_save(self):
+        # check user has an admin attr
+        self.assertTrue(hasattr(self.user, 'admin'))
+        # also a staff attr
+        self.assertTrue(hasattr(self.user, 'staff'))
+        # but not a student attr
+        self.assertFalse(hasattr(self.user, 'student'))
+
+class StaffPostSaveTestCase(BasePostSaveTestCase):
+    state_class = StaffStateFactory
+
+    def test_staff_post_save(self):
+        # check user has a staff attr
+        self.assertTrue(hasattr(self.user, 'staff'))
+        # but not an admin attr
+        self.assertFalse(hasattr(self.user, 'admin'))
+        # or a student attr
+        self.assertFalse(hasattr(self.user, 'student'))
+
+class StudentPostSaveTestCase(BasePostSaveTestCase):
+    state_class = StudentStateFactory
+
+    def test_student_post_save(self):
+        # test user has a student attr
+        self.assertTrue(hasattr(self.user, 'student'))
+        # but not an admin attr
+        self.assertFalse(hasattr(self.user, 'admin'))
+        # or a staff attr
+        self.assertFalse(hasattr(self.user, 'staff'))
+
         # get the student instance
-        student = user.student
-        # check the student has a class_group instance
-        assert hasattr(student, 'class_group')
-        # check the attributes are equal to the state
+        student = self.user.student
+        # check that the student is in the right class as the state said it should be
         self.assertEqual(
-            student.class_group.band.year.value,
-            state.year
+            student.set.band.year.value,
+            self.state.year
         )
         self.assertEqual(
-            student.class_group.band.value,
-            state.band
+            student.set.band.value,
+            self.state.band
         )
         self.assertEqual(
-            student.class_group.value,
-            state.set
+            student.set.value,
+            self.state.set
         )
 
 class BaseAccessMessageMixinTestCase(TestCase):
